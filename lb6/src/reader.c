991a6b6f8bf4e0742856df9bdee73d9a428244d6
elegardooo <112643027+elegardooo@users.noreply.github.com>
2024-06-12 15:34:07 +0300

    Add files via upload

---
+++ lb6/src/reader.c
@@ -0,0 +1,40 @@
+#include "index.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) 
+{
+    if(argc < 1) 
+    {
+        printf("No args!\n");
+        exit(-1);
+    }
+
+    FILE* fd = NULL;
+    if ((fd = fopen(argv[1], "rb")) == NULL)
+    {
+        printf("Open file error\n");
+        exit(-1);
+    }
+
+    index_hdr_t* data = (index_hdr_t*)malloc(sizeof(index_hdr_t));
+
+    fread(&data->records, sizeof(uint64_t), 1, fd);
+
+    data->idx = (index_record_t*)malloc(data->records * sizeof(index_record_t));
+
+    fread(data->idx, sizeof(index_record_t), data->records, fd);
+
+    fclose(fd);
+
+    printf("Records: %lu\n", data->records);
+
+    for (uint64_t i = 0; i < data->records; i++)
+        printf("%lu : %lf\n", i + 1, data->idx[i].time_mark);
+
+    free(data->idx); 
+    free(data); 
+
+    return 0;
+}
